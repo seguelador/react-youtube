36f6ae57666ee9dcd3882713a1fc8c78e4d000e5
Matias Seguel <mseguel@mapcity.cl>
2017-08-01 17:22:31 -0400

    Added components: videolist, videolistitem, videodetail. Working showing first video from fetched data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDs8G9yg5CyMF2zrvqHw46E7jKeFW3iCuo';
 
-//Create a new component. This component should produce HTML
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //this.setState({ videos: videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-//Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
